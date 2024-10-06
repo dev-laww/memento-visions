1652b9347961df7e124950bf2968dc709691b0ac
dev-laww <lawrenceallen.tech@gmail.com>
2024-10-06 18:22:31 +0800

    refactor: moved extensions to its own classes

---
+++ Scripts/Utils/Extensions/IPointExtensions.cs
@@ -0,0 +1,9 @@
+﻿using DelaunatorSharp;
+using Godot;
+
+namespace Game.Utils.Extensions;
+
+public static class IPointExtensions
+{
+    public static Vector2 ToVector(this IPoint point) => new((float)point.X, (float)point.Y);
+}
---
+++ Scripts/Utils/Extensions/Vector2Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Godot;
+
+namespace Game.Utils.Extensions;
+
+public static class Vector2Extensions
+{
+    public static DelaunayPoint ToPoint(this Vector2 vector) => new(vector.X, vector.Y);
+
+    public static float ManhattanDistanceTo(
+        this Vector2 p1,
+        Vector2 p2
+    ) => Math.Abs(p1.X - p2.X) + Math.Abs(p1.Y - p2.Y);
+
+    public static Vector2 SnapToGrid(
+        this Vector2 vector,
+        int snap = 8
+    ) => new(Mathf.Round(vector.X / snap) * snap, Mathf.Round(vector.Y / snap) * snap);
+}
