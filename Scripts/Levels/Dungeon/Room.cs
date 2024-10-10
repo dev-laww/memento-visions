35e4e762bb5a862f9123b3f23534cd23f7da85b5
dev-laww <lawrenceallen.tech@gmail.com>
2024-10-11 00:58:30 +0800

    refactor: rewritten the room node

--- Scripts/Levels/Dungeon/Room.cs
+++ Scripts/Levels/Dungeon/Room.cs
@@ -1,13 +1,34 @@
 using Godot;
 using GodotUtilities;
 
-namespace Game;
+namespace Game.Levels.Dungeon;
 
 [Scene]
 public partial class Room : Node2D
 {
     [Node]
-    private Panel panel;
+    private ColorRect panel;
+
+    private Rect2I bounds;
+
+    public Rect2I Bounds => bounds;
+
+    public Vector2I Center => bounds.Position + bounds.Size / 2;
+
+    public bool Intersects(Room other, int padding = 0)
+    {
+        var thisXMin = bounds.Position.X - padding;
+        var thisXMax = bounds.Position.X + bounds.Size.X + padding;
+        var thisYMin = bounds.Position.Y - padding;
+        var thisYMax = bounds.Position.Y + bounds.Size.Y + padding;
+
+        var otherXMin = other.bounds.Position.X;
+        var otherXMax = other.bounds.Position.X + other.bounds.Size.X;
+        var otherYMin = other.bounds.Position.Y;
+        var otherYMax = other.bounds.Position.Y + other.bounds.Size.Y;
+
+        return !(thisXMax <= otherXMin || thisXMin >= otherXMax || thisYMax <= otherYMin || thisYMin >= otherYMax);
+    }
 
     public override void _Notification(int what)
     {
@@ -16,33 +37,15 @@ public partial class Room : Node2D
         WireNodes();
     }
 
-    public static Room Create(Vector2 position, Vector2 size)
+    public static Room Create(Vector2I position, Vector2I size)
     {
         var room = GD.Load<PackedScene>("res://Scenes/Levels/Dungeon/Room.tscn").Instantiate<Room>();
 
         room.Position = position;
         room.panel.Size = size;
-        room.panel.Position = -size / 2;
+
+        room.bounds = new Rect2I(position, size);
 
         return room;
-    }
-
-    public static RigidBody2D CreateCollider(Vector2 position, Vector2 size)
-    {
-        var collider = new RigidBody2D
-        {
-            CollisionLayer = 20,
-            CollisionMask = 20,
-            LockRotation = true,
-            Freeze = true,
-        };
-
-        var shape = new RectangleShape2D { Size = size };
-        var collision = new CollisionShape2D { Shape = shape };
-        collider.AddChild(collision);
-        collider.SetMeta("size", size);
-        collider.Position = position;
-
-        return collider;
     }
 }
