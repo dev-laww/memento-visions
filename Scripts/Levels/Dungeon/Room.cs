5ffe7df6da6ebc449b7b53a766f784f7ddc15cba
dev-laww <lawrenceallen.tech@gmail.com>
2024-10-11 11:51:02 +0800

    refactor: moved bounds to its own class

--- Scripts/Levels/Dungeon/Room.cs
+++ Scripts/Levels/Dungeon/Room.cs
@@ -9,27 +9,6 @@ public partial class Room : Node2D
     [Node]
     private ColorRect panel;
 
-    private Rect2I bounds;
-
-    public Rect2I Bounds => bounds;
-
-    public Vector2I Center => bounds.Position + bounds.Size / 2;
-
-    public bool Intersects(Room other, int padding = 0)
-    {
-        var thisXMin = bounds.Position.X - padding;
-        var thisXMax = bounds.Position.X + bounds.Size.X + padding;
-        var thisYMin = bounds.Position.Y - padding;
-        var thisYMax = bounds.Position.Y + bounds.Size.Y + padding;
-
-        var otherXMin = other.bounds.Position.X;
-        var otherXMax = other.bounds.Position.X + other.bounds.Size.X;
-        var otherYMin = other.bounds.Position.Y;
-        var otherYMax = other.bounds.Position.Y + other.bounds.Size.Y;
-
-        return !(thisXMax <= otherXMin || thisXMin >= otherXMax || thisYMax <= otherYMin || thisYMin >= otherYMax);
-    }
-
     public override void _Notification(int what)
     {
         if (what != NotificationSceneInstantiated) return;
@@ -43,8 +22,6 @@ public partial class Room : Node2D
 
         room.Position = position;
         room.panel.Size = size;
-
-        room.bounds = new Rect2I(position, size);
 
         return room;
     }
