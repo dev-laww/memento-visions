43dabd7ed8f16789c27f03ef89643146ec045fae
dev-laww <lawrenceallen.tech@gmail.com>
2024-10-14 00:08:45 +0800

    feat: entry points implementation

--- Scripts/Utils/Generation/Bounds.cs
+++ Scripts/Utils/Generation/Bounds.cs
@@ -1,4 +1,5 @@
-﻿using Godot;
+﻿using System.Collections.Generic;
+using Godot;
 
 namespace Game.Utils.Generation;
 
@@ -7,10 +8,22 @@ public class Bounds
     public Rect2I Rect { get; }
 
     public Vector2I Center => Rect.Position + Rect.Size / 2;
+    
+    public List<EntryPoint> EntryPoints { get; } = new();
 
     public Bounds(Vector2I position, Vector2I size)
     {
         Rect = new Rect2I(position, size);
+        
+        var edges = new List<(Vector2I, Vector2I)>
+        {
+            (new Vector2I(Rect.Position.X + Rect.Size.X / 2, Rect.Position.Y), Vector2I.Up),
+            (new Vector2I(Rect.Position.X + Rect.Size.X / 2, Rect.Position.Y + Rect.Size.Y), Vector2I.Down),
+            (new Vector2I(Rect.Position.X, Rect.Position.Y + Rect.Size.Y / 2), Vector2I.Left),
+            (new Vector2I(Rect.Position.X + Rect.Size.X, Rect.Position.Y + Rect.Size.Y / 2), Vector2I.Right),
+        };
+
+        edges.ForEach(edge => EntryPoints.Add(EntryPoint.Create(edge.Item1, edge.Item2)));
     }
 
     public bool Intersects(Bounds other, int padding = 0)
