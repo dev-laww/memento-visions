b4f68f18bea1b0ee85941fb754e4526f53773269
dev-laww <lawrenceallen.tech@gmail.com>
2024-10-14 00:07:08 +0800

    feat: created entry point class

---
+++ Scripts/Utils/Generation/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+
+namespace Game.Utils.Generation;
+
+public class EntryPoint
+{
+    public Vector2I Position { get; }
+    public Vector2I Direction { get; }
+    public bool Open { get; private set; }
+
+    public EntryPoint(Vector2I position, Vector2I direction)
+    {
+        Position = position;
+        Direction = direction;
+    }
+
+    public void Toggle() => Open = !Open;
+
+    public static EntryPoint Create(Vector2I position, Vector2I direction) => new(position, direction);
+}
