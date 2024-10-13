6dae7944fc3f71630304fb56db152d3551e22bb4
dev-laww <lawrenceallen.tech@gmail.com>
2024-10-14 00:11:19 +0800

    feat: added coordinate and entrypoint labeling

--- Scripts/Levels/Dungeon/Room.cs
+++ Scripts/Levels/Dungeon/Room.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Game.Utils.Generation;
 using Godot;
 using GodotUtilities;
 
@@ -7,7 +9,15 @@ namespace Game.Levels.Dungeon;
 public partial class Room : Node2D
 {
     [Node]
-    private ColorRect panel;
+    private ColorRect Background;
+
+    [Node]
+    private Label Label;
+
+    public Bounds Bounds { get; private set; }
+
+
+    public Vector2I Size { get; private set; }
 
     public override void _Notification(int what)
     {
@@ -16,12 +26,21 @@ public partial class Room : Node2D
         WireNodes();
     }
 
-    public static Room Create(Vector2I position, Vector2I size)
+    public void Update()
+    {
+        var entryPointsAsStrings = Bounds.EntryPoints.Where(e => e.Open).Select(point => point.Position.ToString()).ToArray();
+        Label.Text = $"Position: {Bounds.Rect.Position}\nEntry Points: {string.Join("\n ", entryPointsAsStrings)}";
+    }
+
+    public static Room Create(Vector2I position, Vector2I size, Bounds bounds)
     {
         var room = GD.Load<PackedScene>("res://Scenes/Levels/Dungeon/Room.tscn").Instantiate<Room>();
 
         room.Position = position;
-        room.panel.Size = size;
+        room.Bounds = bounds;
+        room.Size = size;
+        room.Background.Size = size;
+        room.Label.Position = size / 2;
 
         return room;
     }
