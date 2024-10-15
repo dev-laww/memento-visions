aaeb7846b7d5afd6e49fd3aa394cc7248b0edee2
dev-laww <lawrenceallen.tech@gmail.com>
2024-10-15 16:29:32 +0800

    feat: stats resource

---
+++ Scripts/Resources/Stats.cs
@@ -0,0 +1,32 @@
+﻿using Godot;
+
+namespace Game.Resources;
+
+[Tool]
+[GlobalClass]
+public partial class Stats: Resource
+{
+    [Export]
+    public int Health { get; set; } = 100;
+
+    [Export]
+    public int MaxHealth { get; set; } = 100;
+    
+    [Export]
+    public int MaxStamina { get; set; } = 100;
+
+    [Export]
+    public int Mana { get; set; } = 100;
+
+    [Export]
+    public int MaxMana { get; set; } = 100;
+
+    [Export]
+    public int Attack { get; set; } = 10;
+
+    [Export]
+    public int Defense { get; set; } = 10;
+
+    [Export]
+    public int Speed { get; set; } = 50;
+}
