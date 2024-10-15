59746b0e24bb8808c682956d158f1acf1fbdc496
dev-laww <lawrenceallen.tech@gmail.com>
2024-10-15 15:20:13 +0800

    enhancement: added item uid

--- Scripts/Resources/Item.cs
+++ Scripts/Resources/Item.cs
@@ -1,4 +1,5 @@
-﻿using Godot;
+﻿using System;
+using Godot;
 
 namespace Game.Resources;
 
@@ -30,13 +31,36 @@ public partial class Item : Resource
     private Texture2D icon { get; set; }
 
     [Export]
-    public Texture2D Sprite { get; set; }
+    public Texture2D sprite { get; set; }
 
     [Export]
     public bool Stackable { get; set; } = true;
 
-    public Texture2D Icon => icon ?? Sprite;
+    public Texture2D Icon
+    {
+        get => GetTexture(icon, sprite);
+    }
+
+    public Texture2D Sprite
+    {
+        get => GetTexture(sprite, icon);
+    }
 
     [Export]
     public int MaxStack { get; set; } = 9999999;
+
+    public readonly Guid UID = Guid.NewGuid();
+
+    public Item()
+    {
+        SetMeta("uid", UID.ToString());
+    }
+
+    private Texture2D GetTexture(Texture2D primary, Texture2D secondary)
+    {
+        if (primary == null && secondary == null)
+            throw new Exception("Item must have a sprite or secondary icon");
+
+        return primary ?? secondary;
+    }
 }
