783e0997d8fae762cb6f345fa104663631b8697a
dev-laww <lawrenceallen.tech@gmail.com>
2024-10-20 03:39:37 +0800

    feat: created node 2d extensions

---
+++ Scripts/Utils/Extensions/Node2DExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Game.Entities.Player;
+using Godot;
+using GodotUtilities;
+
+namespace Game.Utils.Extensions;
+
+public static class Node2DExtensions
+{
+    public static Player GetPlayer(this Node2D node)
+    {
+        var player = node.GetTree().GetNodesInGroup<Player>("Player").FirstOrDefault();
+        
+        if (player == null)
+            GD.PrintErr("Player not found.");
+        
+        return player;
+    }
+}
