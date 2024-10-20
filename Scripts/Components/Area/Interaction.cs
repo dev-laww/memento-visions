b78e367baa2bf61cd0e9acf1b38e97ac86ec8572
dev-laww <lawrenceallen.tech@gmail.com>
2024-10-20 16:25:58 +0800

    feat: interaction system finish

--- Scripts/Components/Area/Interaction.cs
+++ Scripts/Components/Area/Interaction.cs
@@ -1,24 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
 using Game.Globals;
 using Godot;
 using GodotUtilities;
 
-namespace Game.Components.Area;
+namespace Game;
 
-public enum InteractionMode
-{
-    Normal,
-    Deferred,
-}
-
-[Scene]
 [Tool]
+[Scene]
 public partial class Interaction : Area2D
 {
+    [Export]
+    public string InteractionLabel = "Interact";
+    
     [Signal]
     public delegate void InteractedEventHandler();
     
-    public Callable Interact { private get; set; } = Callable.From(() => GD.Print("Interacted"));
-    
+    public Marker2D Marker { get; private set; }
     public override void _Notification(int what) {
         if (what != NotificationSceneInstantiated) return;
         
@@ -29,18 +27,21 @@ public partial class Interaction : Area2D
     {
         BodyEntered += body => InteractionManager.Register(this);
         BodyExited += body => InteractionManager.Unregister(this);
+        Marker = GetChildren().OfType<Marker2D>().First();
     }
+    
+    public void Interact() => EmitSignal(SignalName.Interacted);
 
-    public void InteractWith(InteractionMode mode = InteractionMode.Normal) {
-        EmitSignal(SignalName.Interacted);
+    public override string[] _GetConfigurationWarnings()
+    {
+        var warnings = new List<string>();
 
-        if (mode == InteractionMode.Deferred)
-        {
-            Interact.CallDeferred();
-            return;
-        }
+        var marker = GetChildren().OfType<Marker2D>().FirstOrDefault();
         
-        Interact.Call();
+        if (marker == null)
+            warnings.Add("Marker2D not found.");
+        
+        return warnings.ToArray();
     }
 }
 
--- Scripts/Globals/InteractionManager.cs
+++ Scripts/Globals/InteractionManager.cs
@@ -1,51 +1,78 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Game.Components.Area;
 using Game.Utils.Extensions;
 using Godot;
-using GodotUtilities;
 
 namespace Game.Globals;
 
-[Scene]
 public partial class InteractionManager : Node2D
 {
-    private static InteractionManager Instance { get; set; }
-    private readonly List<Interaction> interactions = new();
-    
-    public override void _Notification(int what) {
-        if (what != NotificationSceneInstantiated) return;
-        
-        WireNodes();
-    }
+    private static InteractionManager instance;
+
+    private readonly List<Interaction> areas = new();
+
+    private HBoxContainer ui;
+
+    private PackedScene InteractionUIScene = GD.Load<PackedScene>("res://Scenes/UI/InteractionUI.tscn");
 
     public override void _Ready()
     {
-        Instance = this;
+        instance = this;
+
+        ui = InteractionUIScene.Instantiate<HBoxContainer>();
+
+        AddChild(ui);
+
+        ui.Hide();
     }
 
     public override void _Process(double delta)
     {
-        if (interactions.Count == 0) return;
+        ui.Hide();
         
-        interactions.Sort((a, b) =>
+        if (areas.Count == 0) return;
+
+        var closest = GetClosest();
+        var label = ui.GetNode<Label>("Label");
+        label.Text = closest.InteractionLabel;
+
+        var icon = ui.GetNode<TextureRect>("Icon");
+        var offset = new Vector2(
+            (icon.Size.X + label.Size.X) / 2,
+            ui.Size.Y / 2
+        );
+
+        ui.GlobalPosition = closest.Marker.GlobalPosition - offset;
+
+        GetTree().CreateTimer(0.2).Timeout += () => ui.Show();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (!@event.IsActionPressed("interact") || areas.Count == 0) return;
+
+        var closest = GetClosest();
+
+        closest.Interact();
+    }
+
+    public static void Register(Interaction area) => instance.areas.Add(area);
+
+    public static void Unregister(Interaction area) => instance.areas.Remove(area);
+
+    private Interaction GetClosest()
+    {
+        if (areas.Count == 0) return null;
+
+        areas.Sort((a, b) =>
         {
             var player = this.GetPlayer();
-            var aDistance = a.GlobalPosition.DistanceTo(player.GlobalPosition);
-            var bDistance = b.GlobalPosition.DistanceTo(player.GlobalPosition);
-            
+            var aDistance = a.Marker.GlobalPosition.DistanceTo(player.GlobalPosition);
+            var bDistance = b.Marker.GlobalPosition.DistanceTo(player.GlobalPosition);
+
             return aDistance.CompareTo(bDistance);
         });
 
-        var closest = interactions.First();
-        
-        if (Input.IsActionPressed("interact"))
-            closest.InteractWith();
+        return areas.First();
     }
-
-
-    public static void Register(Interaction area) => Instance.interactions.Add(area);
-    
-    public static void Unregister(Interaction area) => Instance.interactions.Remove(area);
 }
-
