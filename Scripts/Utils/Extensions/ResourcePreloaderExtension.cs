70b83d9d0b974b92eaaf502a321f47fcea98b381
dev-laww <lawrenceallen.tech@gmail.com>
2024-11-06 21:15:27 +0800

    feat: created resource preloader extension

---
+++ Scripts/Utils/Extensions/ResourcePreloaderExtension.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace Game.Utils.Extensions;
+
+public static class ResourcePreloaderExtension
+{
+    public static T GetResource<T>(
+        this ResourcePreloader loader,
+        string name
+    ) where T : Resource => (T)loader.GetResource(name);
+}
