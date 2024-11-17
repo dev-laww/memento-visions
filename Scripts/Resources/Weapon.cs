7f2a03dedb6d467800c8050208a3e7c10cf30973
dev-laww <lawrenceallen.tech@gmail.com>
2024-11-18 00:00:57 +0800

    feat: created weapon resource

---
+++ Scripts/Resources/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using Godot;
+
+namespace Game.Resources;
+
+[Tool]
+[GlobalClass]
+public partial class Weapon : Item
+{
+    public enum Variant
+    {
+        Dagger,
+        Sword,
+        Gun,
+        Whip
+    }
+
+    [Export]
+    private Variant Type { get; set; } = Variant.Dagger;
+
+    [Export]
+    private SpriteFrames Animations { get; set; }
+
+    [Export]
+    private AudioStream AttackSound { get; set; }
+
+    [Export]
+    private AudioStream HitSound { get; set; }
+
+    public Weapon()
+    {
+        StackSize = (int)StackSizes.Unstackable;
+        NotifyPropertyListChanged();
+    }
+
+    protected override bool IsStackable() => false;
+    protected override void SetStackable(bool value) => NotifyPropertyListChanged();
+}
