d5fb6cf698ede383be3db05df1786057f1a53291
dev-laww <lawrenceallen.tech@gmail.com>
2024-12-27 17:55:43 +0800

    chore: cleanup

--- Scripts/Bar.cs
+++ Scripts/Bar.cs
@@ -4,28 +4,21 @@ using Game.Entities.Player;
 using Game.Utils.Extensions;
 using GodotUtilities;
 
-namespace Game
+namespace Game;
+
+[Scene]
+public partial class Bar : Node2D
 {
-    [Scene]
-    public partial class Bar : Node2D
+    private Player player;
+    private NPC npc;
+
+    public override void _Ready()
     {
-        private Player player;
-        private NPC npc;
+        base._Ready();
+        WireNodes();
 
-        public override void _Ready()
-        {
-            base._Ready();
-            WireNodes();
-
-            // Retrieve Player immediately (likely works fine)
-            player = this.GetPlayer();
-            // npc = GetNode<NPC>("/root/GameManager/CurrentScene/Bar/NPC");
-        }
-
-     
-
-     
-
-
+        // Retrieve Player immediately (likely works fine)
+        player = this.GetPlayer();
+        // npc = GetNode<NPC>("/root/GameManager/CurrentScene/Bar/NPC");
     }
 }
