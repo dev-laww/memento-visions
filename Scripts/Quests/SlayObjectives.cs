bd5ec68d5a5ebcbd9b7191793632338eb4982870
crimson <ambilllKylee@gmail.com>
2024-12-28 22:25:45 +0800

    UPDATE questObjective && add enemy Group

--- Scripts/Quests/SlayObjectives.cs
+++ Scripts/Quests/SlayObjectives.cs
@@ -1,23 +1,40 @@
-using Godot;
-using Game.Enemy.Common;
+using Game.Components.Area;
+ using Game.Components.Managers;
+ using Godot;
+ using GodotUtilities;
+ using Game.Enemy.Common;
+ 
+ namespace Game.Quests
+ {
+     [Tool]
+     [GlobalClass]
+     public partial class SlayObjectives : QuestObjectives
+     {
+         [Export]
+         private string EnemyName;
+         private int amount = 0;
 
-namespace Game.Quests;
+         public override void _Ready()
+         {
+             var enemies = GetTree().GetNodesInGroup("Enemies");
+             foreach (var enemy in enemies)
+             {
+                 if (enemy is Samurai samurai)
+                 {
+                     samurai.EnemyDied += OnEnemyDied;
+                 }
+             }
+         }
 
-[Tool]
-[GlobalClass]
-public partial class SlayObjectives : QuestObjectives
-{
-    [Export]
-    private string EnemyName;
-
-    private int amount = 0;
-    private Samurai samurai;
-
-    public override void _Ready() { }
-
-    public void OnEnemyDied(string EnemyKilled)
-    {
-        if (EnemyKilled != EnemyName) return;
-        if (++amount == TargetCount) ObjectiveComplete();
-    }
-}
+         public void OnEnemyDied (string EnemyKilled)
+         {
+             GD.Print("Enemy Killed: " + EnemyKilled + " Enemy Name: " + EnemyName);
+             if (EnemyKilled != EnemyName) return;
+             if (++amount == TargetCount)
+             {
+                 GD.Print(amount);
+                 ObjectiveComplete();
+             }
+         }
+     }
+ }
