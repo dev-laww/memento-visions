fa3e47b66533bf490594a533c34546eacfb077e6
dev-laww <lawrenceallen.tech@gmail.com>
2024-12-30 15:05:14 +0800

    fix: minor bug fix

--- Scripts/Components/Managers/StatsManager.cs
+++ Scripts/Components/Managers/StatsManager.cs
@@ -89,7 +89,7 @@ public partial class StatsManager : Node
         set => maxMana = value;
     }
 
-    public Attack MagicalAttack => Utils.Battle.Attack.Magical(Attack);
+    public Attack MagicalAttack => Utils.Battle.Attack.Magical(Magic);
     public Attack PhysicalAttack => Utils.Battle.Attack.Physical(Attack);
     public float PhysicalDamageMultiplier => Stats.PhysicalDamageMultiplier;
     public float MagicalDamageMultiplier => Stats.MagicalDamageMultiplier;
