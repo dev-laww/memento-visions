8381f570aa3b2621720f3f5a1dbfe54b8ddff24c
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-12 16:49:11 +0800

    enhancement: added warnings on transition area

--- Scripts/Components/Area/TransitionArea.cs
+++ Scripts/Components/Area/TransitionArea.cs
@@ -1,33 +1,49 @@
 using Godot;
-using System;
 using Game.Components.Managers;
 using Game.Entities.Player;
-using Game.Globals;
-using Game.Utils.Extensions;
+
+
+namespace Game.Components.Area;
 
 [Tool]
 [GlobalClass]
 public partial class TransitionArea : Area2D
 {
-	
-	[Export] public string TargetScenePath;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		var Player = this.GetPlayer();
-		BodyEntered += OnBodyEntered;
-	}
+    [Export]
+    public string TargetScene
+    {
+        get => targetScene;
+        set
+        {
+            targetScene = value;
+            NotifyPropertyListChanged();
+        }
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-	
-	private void OnBodyEntered(Node body)
-	{
-		if (body is Player player)
-		{
-			GameManager.ChangeScene(TargetScenePath, Vector2.Zero, Transition.Fade);
-		}
-	}
+    private string targetScene;
+
+    public override void _Ready()
+    {
+        CollisionLayer = 0;
+        CollisionMask = 1 << 2;
+        BodyEntered += OnBodyEntered;
+        NotifyPropertyListChanged();
+    }
+
+    private void OnBodyEntered(Node body)
+    {
+        if (body is not Player) return;
+
+        GameManager.ChangeScene(targetScene);
+    }
+
+    public override string[] _GetConfigurationWarnings()
+    {
+        var warnings = new System.Collections.Generic.List<string>();
+
+        if (TargetScene == null)
+            warnings.Add("TargetScene is not set.");
+
+        return warnings.ToArray();
+    }
 }
