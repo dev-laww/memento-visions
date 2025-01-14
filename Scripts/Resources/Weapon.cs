8e51148d747f97745ca28cb718858a2dbaa9c42e
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-14 16:47:47 +0800

    refactor: moved enum inside class

--- Scripts/Resources/Weapon.cs
+++ Scripts/Resources/Weapon.cs
@@ -2,16 +2,16 @@ using Godot;
 
 namespace Game.Resources;
 
-public enum WeaponType
-{
-    Dagger,
-    Sword,
-    Gun,
-    Whip,
-}
-
 [GlobalClass, Icon("res://assets/icons/weapon.svg")]
 public partial class Weapon : Item
 {
-    [Export] public WeaponType Type;
+    public enum Type
+    {
+        Dagger,
+        Sword,
+        Gun,
+        Whip,
+    }
+
+    [Export] public Type WeaponType;
 }
