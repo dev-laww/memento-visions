8fc6d0c8d3cc5b8eab52bb27aac336caaf7f1577
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-14 16:49:43 +0800

    feat: updated inventory manager

--- Scripts/Components/Managers/InventoryManager.cs
+++ Scripts/Components/Managers/InventoryManager.cs
@@ -7,24 +7,46 @@ namespace Game.Components.Managers;
 
 public partial class InventoryManager : Node
 {
-    [Signal] public delegate void ItemAddedEventHandler(Item item);
+    [Signal] public delegate void UpdatedEventHandler(ItemGroup item);
 
-    private readonly ReadOnlyDictionary<ItemCategory, List<Item>> Inventory = new(
-        new Dictionary<ItemCategory, List<Item>>
+    private readonly ReadOnlyDictionary<Item.Category, List<ItemGroup>> Inventory = new(
+        new Dictionary<Item.Category, List<ItemGroup>>
         {
-            { ItemCategory.Weapon, [] },
-            { ItemCategory.Quest, [] },
-            { ItemCategory.Consumable, [] },
-            { ItemCategory.Material, [] }
+            { Item.Category.Weapon, [] },
+            { Item.Category.Quest, [] },
+            { Item.Category.Consumable, [] },
+            { Item.Category.Material, [] }
         }
     );
 
-    private void AddItem(Item item)
+    public void AddItem(Item item, int quantity = 1)
     {
-        Inventory[item.Category].Add(item);
-        EmitSignal(SignalName.ItemAdded, item);
+        var itemGroup = Inventory[item.ItemCategory].Find(group => group.Item.UniqueName == item.UniqueName);
+
+        if (itemGroup is not null)
+            itemGroup.Quantity += quantity;
+        else
+        {
+            itemGroup = new ItemGroup { Item = item, Quantity = quantity };
+            Inventory[item.ItemCategory].Add(itemGroup);
+        }
+
+        EmitSignal(SignalName.Updated, itemGroup);
     }
 
-    private void RemoveItem(Item item) => Inventory[item.Category].Remove(item);
-    public bool HasItem(Item item) => Inventory[item.Category].Contains(item);
+    public void RemoveItem(Item item, int quantity = 1)
+    {
+        var itemGroup = Inventory[item.ItemCategory].Find(group => group.Item.UniqueName == item.UniqueName);
+
+        if (itemGroup is null) return;
+
+        itemGroup.Quantity -= quantity;
+
+        if (itemGroup.Quantity <= 0)
+            Inventory[item.ItemCategory].Remove(itemGroup);
+
+        EmitSignal(SignalName.Updated, itemGroup);
+    }
+
+    public IReadOnlyList<ItemGroup> GetItemsFromCategory(Item.Category category) => Inventory[category].AsReadOnly();
 }
