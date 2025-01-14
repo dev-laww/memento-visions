15f9d5bd9fefa150d3a63c2848865c707750e66f
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-14 21:09:08 +0800

    feat: added utility methods

--- Scripts/Resources/Recipe.cs
+++ Scripts/Resources/Recipe.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using Game.Globals;
 using Godot;
 
 namespace Game.Resources;
@@ -20,7 +21,7 @@ public partial class Recipe : Resource
 
     public bool Unlocked;
 
-    public ItemGroup[] GetIngredients(int quantity = 1) =>
+    private ItemGroup[] GetIngredients(int quantity = 1) =>
         quantity switch
         {
             < 1 => throw new ArgumentOutOfRangeException(nameof(quantity), "Quantity must be greater than 0."),
@@ -29,4 +30,26 @@ public partial class Recipe : Resource
                 new ItemGroup { Item = ingredient.Item, Quantity = ingredient.Quantity * quantity }
             ).ToArray()
         };
+
+    public bool CanCreate(int quantity = 1)
+    {
+        var ingredients = GetIngredients(quantity);
+        
+        return ingredients.All(ingredient =>
+            PlayerInventoryManager.HasItem(ingredient.Item, ingredient.Quantity)
+        );
+    }
+    
+    public void Create(int quantity = 1)
+    {
+        if (!CanCreate(quantity)) return;
+
+        var ingredients = GetIngredients(quantity);
+
+        foreach (var ingredient in ingredients)
+            PlayerInventoryManager.RemoveItem(ingredient.Item, ingredient.Quantity);
+
+        PlayerInventoryManager.AddItem(Result, quantity);
+    }
+    
 }
