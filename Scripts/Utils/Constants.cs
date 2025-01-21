7a21ccb0837e7d8a149b70555917447d611e5710
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-21 21:36:50 +0800

    feat: constants update

--- Scripts/Utils/Constants.cs
+++ Scripts/Utils/Constants.cs
@@ -2,6 +2,8 @@ namespace Game.Utils;
 
 public class Constants
 {
-    public const string RECIPES_PATH = "res://resources/recipes/";
+    public const string RECIPES_PATH = "res://resources/recipes";
+    public const string ITEMS_PATH = "res://resources/items";
+    public const string WEAPONS_PATH = "res://resources/weapons";
     public const string SAVE_NAME = "save.json";
 }
