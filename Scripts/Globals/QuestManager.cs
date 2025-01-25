45d47b8b2ed9bec38c62751e8a8e3c7bb90040c6
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-25 23:19:15 +0800

    feat: added signals for quest manager

--- Scripts/Globals/QuestManager.cs
+++ Scripts/Globals/QuestManager.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using Game.Resources;
+using Godot;
 
 namespace Game.Globals;
 
 public partial class QuestManager : Global<QuestManager>
 {
-    public class ActiveQuest
+    public partial class ActiveQuest : GodotObject
     {
         public Quest Quest;
         public int Step;
@@ -13,9 +14,19 @@ public partial class QuestManager : Global<QuestManager>
         public bool Completed;
     }
 
+    [Signal] public delegate void QuestAddedEventHandler(ActiveQuest activeQuest);
+    [Signal] public delegate void QuestUpdatedEventHandler(ActiveQuest activeQuest);
+    [Signal] public delegate void QuestCompletedEventHandler(ActiveQuest activeQuest);
+
     private readonly List<ActiveQuest> activeQuests = [];
 
     public static IReadOnlyList<ActiveQuest> ActiveQuests => Instance.activeQuests;
 
-    public static void Add(Quest quest) => Instance.activeQuests.Add(new ActiveQuest { Quest = quest, Step = 0, Progress = 0, Completed = false });
+    public static void Add(Quest quest)
+    {
+        Instance.activeQuests.Add(new ActiveQuest { Quest = quest, Step = 0, Progress = 0, Completed = false });
+        Instance.EmitSignal(SignalName.QuestAdded, Instance.activeQuests[^1]);
+    }
+    
+    // TODO: Implement the rest of the QuestManager class
 }
