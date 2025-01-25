05ae063ae16f9cd6024712a8395f2a2b4f6c2737
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-25 21:14:02 +0800

    feat: created quest resource

--- Scripts/Resources/ItemGroup.cs
+++ Scripts/Resources/ItemGroup.cs
@@ -2,12 +2,13 @@ using Godot;
 
 namespace Game.Resources;
 
+[Tool]
 [GlobalClass, Icon("res://assets/icons/item-group.svg")]
 public partial class ItemGroup : Resource
 {
     [Export] public Item Item;
     [Export] public int Quantity = 1;
-    
+
     public void Deconstruct(out Item item, out int quantity)
     {
         item = Item;
--- Scripts/Resources/Quest.cs
+++ Scripts/Resources/Quest.cs
@@ -1,6 +1,16 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace Game.Resources;
 
+[Tool]
 [GlobalClass, Icon("res://assets/icons/quest.svg")]
-public partial class Quest : Resource { }
+public partial class Quest : Resource
+{
+    [Export] public string Title;
+    [Export(PropertyHint.MultilineText)] public string Description;
+
+    [Export] private QuestStep[] steps = [];
+
+    public IReadOnlyList<QuestStep> Steps => [.. steps];
+}
