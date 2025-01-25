b0e6f73e9f85963133f5cbb4d301af6f2c329606
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-25 19:15:45 +0800

    feat: add Quest and QuestStep resource classes with properties for quest management

---
+++ Scripts/Resources/Quest.cs
@@ -0,0 +1,6 @@
+using Godot;
+
+namespace Game.Resources;
+
+[GlobalClass, Icon("res://assets/icons/quest.svg")]
+public partial class Quest : Resource { }
---
+++ Scripts/Resources/QuestStep.cs
@@ -0,0 +1,57 @@
+using Godot;
+using Godot.Collections;
+
+namespace Game.Resources;
+
+[Tool]
+[GlobalClass, Icon("res://assets/icons/quest-step.svg")]
+public partial class QuestStep : Resource
+{
+    public enum StepType { CollectItem, Navigate, KillEnemy, GiveItem }
+
+    [Export]
+    public StepType Type
+    {
+        get => type;
+        set
+        {
+            type = value;
+            NotifyPropertyListChanged();
+        }
+    }
+
+    [Export] public string Description { get; set; }
+
+    public Item Item { get; set; }
+    public int Count { get; set; } = 1;
+
+    // TODO: add properties for enemy
+
+    private StepType type;
+    public override Array<Dictionary> _GetPropertyList()
+    {
+        var properties = new Array<Dictionary>();
+
+        switch (Type)
+        {
+            case StepType.CollectItem:
+            case StepType.GiveItem:
+                properties.Add(new Dictionary
+                {
+                    { "name", nameof(Item) },
+                    { "type", (int)Variant.Type.String },
+                    { "usage", (int)PropertyUsageFlags.Default }
+                });
+
+                properties.Add(new Dictionary
+                {
+                    { "name", nameof(Count) },
+                    { "type", (int)Variant.Type.Int },
+                    { "usage", (int)PropertyUsageFlags.Default }
+                });
+                break;
+        }
+
+        return properties;
+    }
+}
