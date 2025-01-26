d739bf3d02f82b545b42ebb5e45ba82929b31b00
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-26 22:33:28 +0800

    feat: created id inspector plugin for managing ids

---
+++ addons/Project Plugins/InspectorPlugins/IdInspectorPlugin.cs
@@ -0,0 +1,95 @@
+using Game.Registry;
+using Game.Resources;
+using Godot;
+
+namespace ProjectPlugin.InspectorPlugin;
+
+public partial class IdInspectorPlugin : EditorInspectorPlugin
+{
+    public override bool _CanHandle(GodotObject @object) => @object is Quest or Item;
+
+    public override bool _ParseProperty(GodotObject @object, Variant.Type type, string name, PropertyHint hintType, string hintString, PropertyUsageFlags usageFlags, bool wide)
+    {
+        if (name != "Id") return false;
+
+        var hbox = new HBoxContainer { SizeFlagsHorizontal = Control.SizeFlags.ExpandFill };
+        var labelHbox = new HBoxContainer { SizeFlagsHorizontal = Control.SizeFlags.ExpandFill };
+        var editor = new HBoxContainer { SizeFlagsHorizontal = Control.SizeFlags.ExpandFill };
+        var label = new Label { Text = "Id", SizeFlagsHorizontal = Control.SizeFlags.ExpandFill };
+
+        labelHbox.AddChild(label);
+        hbox.AddChild(labelHbox);
+        hbox.AddChild(editor);
+
+        var lineEdit = new LineEdit
+        {
+            Text = @object.Get(name).ToString(),
+            Editable = @object.Get(name).ToString() == string.Empty,
+            SizeFlagsHorizontal = Control.SizeFlags.ExpandFill
+        };
+
+        Button createButton()
+        {
+            var button = new Button { Text = "Reset" };
+            button.Pressed += () =>
+            {
+                @object.Set(name, "");
+                lineEdit.Text = "";
+                lineEdit.Editable = true;
+                lineEdit.GrabFocus();
+
+                button.QueueFree();
+
+                var obj = @object as Resource;
+                ResourceSaver.Save(obj, obj.ResourcePath);
+            };
+
+            return button;
+        }
+
+        void SubmitText(string text)
+        {
+            if (text == string.Empty)
+            {
+                GD.PrintErr("Id cannot be empty.");
+                return;
+            }
+
+            var existing = ItemRegistry.Get(text) as Resource ?? QuestRegistry.Get(text);
+            var obj = @object as Resource;
+
+            if (existing != null && existing.ResourcePath != obj.ResourcePath)
+            {
+                GD.PrintErr($"Id '{text}' already exists. Please choose a different id. {existing.ResourcePath}");
+                return;
+            }
+
+            @object.Set(name, text);
+            lineEdit.Text = text;
+            lineEdit.Editable = false;
+
+            if (labelHbox.GetChildCount() != 1) return;
+
+            labelHbox.AddChild(createButton());
+        }
+
+        lineEdit.TextSubmitted += SubmitText;
+        lineEdit.TextChanged += text => @object.Set(name, text);
+        lineEdit.FocusExited += () =>
+        {
+            if (!lineEdit.Editable) return;
+
+            SubmitText(lineEdit.Text);
+        };
+
+
+        if (!lineEdit.Editable && labelHbox.GetChildCount() == 1)
+            labelHbox.AddChild(createButton());
+
+        editor.AddChild(lineEdit);
+
+        AddCustomControl(hbox);
+
+        return true;
+    }
+}
--- addons/Project Plugins/ProjectPlugin.cs
+++ addons/Project Plugins/ProjectPlugin.cs
@@ -8,15 +8,20 @@ namespace ProjectPlugin;
 public partial class ProjectPlugin : EditorPlugin
 {
     private DungeonInspectorPlugin _inspectorPlugin;
+    private IdInspectorPlugin _idInspectorPlugin;
 
     public override void _EnterTree()
     {
         _inspectorPlugin = new DungeonInspectorPlugin();
+        _idInspectorPlugin = new IdInspectorPlugin();
+
         AddInspectorPlugin(_inspectorPlugin);
+        AddInspectorPlugin(_idInspectorPlugin);
     }
 
     public override void _ExitTree()
     {
         RemoveInspectorPlugin(_inspectorPlugin);
+        RemoveInspectorPlugin(_idInspectorPlugin);
     }
 }
