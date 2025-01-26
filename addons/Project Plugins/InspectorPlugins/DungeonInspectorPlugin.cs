1f23eb1d02b18b4d9f565560fdf9e257008c83e5
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-26 22:33:52 +0800

    chore: cleanup

--- addons/Project Plugins/InspectorPlugins/DungeonInspectorPlugin.cs
+++ addons/Project Plugins/InspectorPlugins/DungeonInspectorPlugin.cs
@@ -6,31 +6,19 @@ namespace ProjectPlugin.InspectorPlugin;
 [Tool]
 public partial class DungeonInspectorPlugin : EditorInspectorPlugin
 {
-    public override bool _CanHandle(GodotObject @object)
-    {
-        // Check if the object is of type Dungeon
-        return @object is Dungeon;
-    }
+    public override bool _CanHandle(GodotObject @object) => @object is Dungeon;
 
     public override void _ParseCategory(GodotObject @object, string category)
     {
         // Cast the object to Dungeon
         if (@object is not Dungeon dungeon || category != "Dungeon") return;
 
-
         // Create a new button
-        var button = new Button();
-        button.Text = "Generate";
+        var button = new Button { Text = "Generate" };
         button.Pressed += () => OnGenerateButtonPressed(dungeon);
 
         // Add the button to the inspector
         AddCustomControl(button);
     }
-
-    private void OnGenerateButtonPressed(Dungeon dungeon)
-    {
-        // Ensure the dungeon instance is valid
-        // Call the Generate method on the Dungeon instance
-        dungeon?.Generate();
-    }
+    private static void OnGenerateButtonPressed(Dungeon dungeon) => dungeon.Generate();
 }
