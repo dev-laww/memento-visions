f1d4c57d3457fcf80b809fbcf675ab1efc702ff4
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-26 22:34:55 +0800

    feat: added quest path on constants

--- Scripts/Utils/Constants.cs
+++ Scripts/Utils/Constants.cs
@@ -2,6 +2,7 @@ namespace Game.Utils;
 
 public class Constants
 {
+    public const string QUESTS_PATH = "res://resources/quests";
     public const string RECIPES_PATH = "res://resources/recipes";
     public const string ITEMS_PATH = "res://resources/items";
     public const string SAVE_NAME = "save.json";
