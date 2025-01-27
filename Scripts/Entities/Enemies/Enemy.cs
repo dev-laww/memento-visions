b506b4e351b744dd02fd4329aaf49396a85b9f5d
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-27 21:12:33 +0800

    feat: created base enemy

---
+++ Scripts/Entities/Enemies/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using Game.Globals;
+using Godot;
+
+namespace Game.Entities.Enemies;
+
+public abstract partial class Enemy : Entity
+{
+    [Export] private string Id;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        EnemyManager.Register(this);
+    }
+
+    protected override void Die(Entity entity)
+    {
+        base.Die(entity);
+
+        EnemyManager.Unregister(this);
+    }
+}
