c9280edd0b3ef7306896370af787a15289820d1d
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-27 21:12:39 +0800

    feat: created enemy manager

---
+++ Scripts/Globals/EnemyManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Game.Entities.Enemies;
+using Godot;
+
+namespace Game.Globals;
+
+public partial class EnemyManager : Global<EnemyManager>
+{
+    [Signal] public delegate void SpawnedEventHandler(Enemy enemy);
+    [Signal] public delegate void DiedEventHandler(Enemy enemy);
+
+    public static event SpawnedEventHandler EnemySpawned
+    {
+        add => Instance.Spawned += value;
+        remove => Instance.Spawned -= value;
+    }
+
+    public static event DiedEventHandler EnemyDied
+    {
+        add => Instance.Died += value;
+        remove => Instance.Died -= value;
+    }
+
+    public readonly List<Enemy> enemies = [];
+
+    public static IReadOnlyList<Enemy> Enemies => Instance.enemies;
+    public static int EnemyCount => Instance.enemies.Count;
+
+    public static void Register(Enemy enemy)
+    {
+        Instance.enemies.Add(enemy);
+        Instance.EmitSignal(SignalName.Spawned, enemy);
+    }
+
+    public static void Unregister(Enemy enemy)
+    {
+        Instance.enemies.Remove(enemy);
+        Instance.EmitSignal(SignalName.Died, enemy);
+    }
+}
