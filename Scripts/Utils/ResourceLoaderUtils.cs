54509b874384131ae7d6d5ed37388545bf54aab4
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-28 00:44:33 +0800

    feat: created resource loader utils

---
+++ Scripts/Utils/ResourceLoaderUtils.cs
@@ -0,0 +1,18 @@
+﻿using Godot;
+
+namespace Game.Utils;
+
+public static class ResourceLoaderUtils
+{
+    public static bool Load(string path, out GodotObject resource)
+    {
+        resource = ResourceLoader.Load(path);
+        return resource != null;
+    }
+
+    public static bool Load<T>(string path, out T resource) where T : GodotObject
+    {
+        resource = ResourceLoader.Load<T>(path);
+        return resource != null;
+    }
+}
