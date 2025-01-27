82cc6b4bcc9872144c3433a5343ccc078dc79970
dev-laww <lawrenceallen.tech@gmail.com>
2025-01-28 00:46:37 +0800

    feat: created enemy registry

---
+++ Scripts/Registry/EnemyRegistry.cs
@@ -0,0 +1,8 @@
+﻿using Game.Resources;
+using Game.Utils;
+using Godot;
+
+namespace Game.Registry;
+
+[GlobalClass]
+public partial class EnemyRegistry() : Registry<Enemy, EnemyRegistry>(Constants.ENEMIES_PATH);
--- Scripts/Utils/Constants.cs
+++ Scripts/Utils/Constants.cs
@@ -5,5 +5,6 @@ public class Constants
     public const string QUESTS_PATH = "res://resources/quests";
     public const string RECIPES_PATH = "res://resources/recipes";
     public const string ITEMS_PATH = "res://resources/items";
+    public const string ENEMIES_PATH = "res://resources/enemies";
     public const string SAVE_NAME = "save.json";
 }
