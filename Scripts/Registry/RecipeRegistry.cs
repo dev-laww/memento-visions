83c1836bc43a15672f3e3fbbf55598272e725ca3
dev-laww <lawrenceallen.tech@gmail.com>
2025-02-01 21:53:16 +0800

    refactor: changed the recipe registry to use registry abstract class

--- Scripts/Registry/RecipeRegistry.cs
+++ Scripts/Registry/RecipeRegistry.cs
@@ -1,31 +1,16 @@
 using System.Collections.Generic;
 using System.Linq;
-using FuzzySharp;
 using Game.Common;
-using Game.Common.Utilities;
+using Game.Common.Abstract;
 using Game.Resources;
 using Godot;
 
 namespace Game.Registry;
 
 [GlobalClass]
-public partial class RecipeRegistry : GodotObject
+public partial class RecipeRegistry() : Registry<Recipe, RecipeRegistry>(Constants.RECIPES_PATH)
 {
-    private static readonly List<string> recipes = DirAccessUtils.GetFilesRecursively(Constants.RECIPES_PATH);
-
-    public static Recipe Get(string id) => (
-        from recipe in Engine.IsEditorHint() ? DirAccessUtils.GetFilesRecursively(Constants.RECIPES_PATH) : recipes
-        where Fuzz.PartialRatio(recipe.Split("/").Last(), id.Split(":").Last()) >= 80
-        select ResourceLoader.Load<Recipe>(recipe)
-        into resource
-        where resource.Result.Item.Id == id
-        select resource
-    ).FirstOrDefault();
-
-    public static List<Recipe> GetRecipes(Recipe.Type? type = null) =>
-    [
-        .. recipes.Select(
-            recipe => ResourceLoader.Load<Recipe>(recipe)
-        ).Where(resource => type == null || resource.RecipeType == type)
-    ];
+    public static List<Recipe> GetRecipes(Recipe.Type type) => [.. GetFileCache().SelectMany(pair => pair.Value)
+        .Select(file => ResourceLoader.Load<Recipe>(file))
+        .Where(recipe => recipe.RecipeType == type)];
 }
