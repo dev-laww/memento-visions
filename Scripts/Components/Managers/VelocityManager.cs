bec124dadb98f3cfb56c2a68d611a784621de8de
dev-laww <lawrenceallen.tech@gmail.com>
2025-02-01 15:12:31 +0800

    feat: implemented core function of velocity manager

--- Scripts/Components/Managers/VelocityManager.cs
+++ Scripts/Components/Managers/VelocityManager.cs
@@ -1,5 +1,6 @@
 using Game.Utils.Extensions;
 using Godot;
+using Godot.Collections;
 
 namespace Game.Components.Managers;
 
@@ -7,53 +8,101 @@ namespace Game.Components.Managers;
 [GlobalClass]
 public partial class VelocityManager : Node
 {
-    [Export] public StatsManager StatsManager;
-    [Export] public float AccelerationCoefficient = 10;
+    [Export] private StatsManager StatsManager;
+    [Export] private float AccelerationCoefficient = 10;
+
+    [ExportCategory("Dash")]
+    [Export(PropertyHint.Range, "0,5,1")]
+    private int TimesCanDash = 1;
+
+    [Export] private float DashSpeed = 200;
+    [Export] private float DashAccelerationCoefficient = 20;
+
+    [Signal] public delegate void DashedEventHandler(Vector2 position);
+    [Signal] public delegate void DashFreedEventHandler(Vector2 position);
 
     public Vector2 Velocity { get; private set; }
-    private float MaxSpeed => StatsManager.Speed;
-    private bool signalEmitted;
+    public CharacterBody2D Body => Owner as CharacterBody2D;
+    public Vector2 LastFacedDirection { get; private set; } = Vector2.Down;
+    public bool CanDash => dashQueue.Count < TimesCanDash;
 
-    [Signal] public delegate void AcceleratingEventHandler();
-    [Signal] public delegate void DeceleratingEventHandler();
+    public bool IsDashing
+    {
+        get => isDashing;
+        set
+        {
+            if (isDashing == value) return;
+
+            isDashing = value;
+
+            if (value) EmitSignal(SignalName.Dashed, Body.GlobalPosition);
+        }
+    }
+
+    private Array<Vector2> dashQueue = [];
+    private float MaxSpeed => StatsManager.Speed;
+    private bool isDashing;
 
     public override void _Ready()
     {
         SetProcess(false);
     }
 
-    public void Accelerate(Vector2 direction) => AccelerateToVelocity(direction.TryNormalize() * MaxSpeed);
-
-    public void Decelerate(bool force = false)
+    public VelocityManager Accelerate(Vector2 direction)
     {
-        if (signalEmitted)
-        {
-            EmitSignal(SignalName.Decelerating);
-            signalEmitted = true;
-        }
-
-        if (force)
-        {
-            Velocity = Vector2.Zero;
-            return;
-        }
-
-        AccelerateToVelocity(Vector2.Zero);
+        LastFacedDirection = direction;
+        return AccelerateToVelocity(direction.TryNormalize() * MaxSpeed);
     }
 
-    public void AccelerateToVelocity(Vector2 velocity)
+    public VelocityManager Decelerate(bool force = false)
     {
+        if (!force) return AccelerateToVelocity(Vector2.Zero);
+
+        Velocity = Vector2.Zero;
+        return this;
+    }
+
+    public VelocityManager AccelerateToVelocity(Vector2 velocity, float accelerationCoefficient = 0)
+    {
+        accelerationCoefficient = accelerationCoefficient == 0 ? AccelerationCoefficient : accelerationCoefficient;
         var delta = (float)GetProcessDeltaTime();
-        var weight = 1f - Mathf.Exp(-AccelerationCoefficient * delta);
+        var weight = 1f - Mathf.Exp(-accelerationCoefficient * delta);
         Velocity = Velocity.Lerp(velocity, weight);
-        ApplyMovement();
+
+        return this;
     }
 
-    private void ApplyMovement()
+    public VelocityManager Dash(Vector2 direction = default)
+    {
+        if (direction == default) direction = LastFacedDirection;
+        if (!CanDash) return this;
+
+        dashQueue.Add(direction);
+        IsDashing = true;
+
+        return this;
+    }
+
+    public void ApplyMovement()
     {
         if (Owner is not CharacterBody2D owner) return;
 
         owner.Velocity = Velocity;
         owner.MoveAndSlide();
+    }
+
+    private void OnDashCooldownTimeout()
+    {
+        if (dashQueue.Count == 0) return;
+
+        var direction = dashQueue[^1];
+        dashQueue.RemoveAt(dashQueue.Count - 1);
+        EmitSignal(SignalName.DashFreed, direction);
+    }
+
+    private void OnDashDurationTimeout()
+    {
+        var lastDash = dashQueue.Count > 0 ? dashQueue[^1] : Vector2.Zero;
+        EmitSignal(SignalName.Dashed, lastDash);
     }
 }
