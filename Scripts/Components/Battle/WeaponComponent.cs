f6a86d66eb2fdf5cb0bcd73578f608cb4faf1388
dev-laww <lawrenceallen.tech@gmail.com>
2025-02-03 22:08:44 +0800

    feat: enhance WeaponComponent with audio and animation properties, and improve node management

--- Scripts/Components/Battle/WeaponComponent.cs
+++ Scripts/Components/Battle/WeaponComponent.cs
@@ -1,35 +1,97 @@
-﻿using Godot;
+﻿using System.Collections.Generic;
+using Game.Common.Extensions;
+using Godot;
+using GodotUtilities;
 
 namespace Game.Components.Battle;
 
 // TODO: implement sound effects, collision shape animations, attack pattern animations
 [Tool]
+[Scene]
 [GlobalClass, Icon("res://assets/icons/weapon-component.svg")]
 public partial class WeaponComponent : Node2D
 {
-    // TODO: Implement this
-    [Export] private SpriteFrames Animations { get; set; }
-    [Export] private AudioStream AttackSound { get; set; }
-    [Export] private AudioStream HitSound { get; set; }
+    [Export]
+    private SpriteFrames Animations
+    {
+        get => animatedSprite2d?.SpriteFrames;
+        set
+        {
+            if (animatedSprite2d == null) return;
 
-    public AnimationPlayer AnimationPlayer => GetNodeOrNull<AnimationPlayer>("AnimationPlayer");
-    public void Animate(string direction) => AnimationPlayer.Play(direction);
+            animatedSprite2d.SpriteFrames = value;
+            animatedSprite2d.NotifyPropertyListChanged();
+        }
+    }
+
+    [Export]
+    private AudioStream AttackSound
+    {
+        get => attackSfx?.Stream;
+        set
+        {
+            if (attackSfx == null) return;
+
+            attackSfx.Stream = value;
+            attackSfx.NotifyPropertyListChanged();
+        }
+    }
+
+    [Export]
+    private AudioStream HitSound
+    {
+        get => hitSfx?.Stream;
+        set
+        {
+            if (hitSfx == null) return;
+
+            hitSfx.Stream = value;
+            hitSfx.NotifyPropertyListChanged();
+        }
+    }
+
+    [Node] private SmoothAnimatedSprite2D animatedSprite2d;
+    [Node] private AnimationPlayer animationPlayer;
+    [Node] private AudioStreamPlayer2D attackSfx;
+    [Node] private AudioStreamPlayer2D hitSfx;
+
+
+    public SignalAwaiter AnimationFinished => ToSignal(animationPlayer, "animation_finished");
+
+    public void Animate(string direction) => animationPlayer.Play(direction);
+
+    public override void _Notification(int what)
+    {
+        if (what != NotificationSceneInstantiated) return;
+
+        WireNodes();
+    }
+
+    public override string[] _GetConfigurationWarnings()
+    {
+        var warnings = new List<string>();
+
+        if (Animations == null)
+            warnings.Add("SpriteFrames is not set.");
+
+        if (AttackSound == null)
+            warnings.Add("AttackSound is not set.");
+
+        if (HitSound == null)
+            warnings.Add("HitSound is not set.");
+
+        return [.. warnings];
+    }
 
     public override void _EnterTree()
     {
         if (GetNodeOrNull("Assets") != null) return;
 
-        var assets = new Node2D();
-        var spriteNode = new SmoothAnimatedSprite2D();
-        var playerNode = new AnimationPlayer();
-        var attackSfxNode = new AudioStreamPlayer2D();
-        var hitSfxNode = new AudioStreamPlayer2D();
-
-        assets.Name = "Assets";
-        spriteNode.Name = nameof(SmoothAnimatedSprite2D);
-        playerNode.Name = nameof(AnimationPlayer);
-        attackSfxNode.Name = "AttackSfx";
-        hitSfxNode.Name = "HitSfx";
+        var assets = new Node2D { Name = "Assets" };
+        var sprite = new SmoothAnimatedSprite2D { Name = "AnimatedSprite2D" };
+        var animationPlayer = new AnimationPlayer { Name = "AnimationPlayer" };
+        var attackSfx = new AudioStreamPlayer2D { Name = "AttackSfx" };
+        var hitSfx = new AudioStreamPlayer2D { Name = "HitSfx" };
 
         var library = new AnimationLibrary();
 
@@ -38,20 +100,18 @@ public partial class WeaponComponent : Node2D
         library.AddAnimation("left", new Animation());
         library.AddAnimation("right", new Animation());
 
-        playerNode.AddAnimationLibrary("", library);
+        animationPlayer.AddAnimationLibrary("", library);
 
-        AddChild(assets);
-        AddChild(playerNode);
-        assets.AddChild(spriteNode);
-        assets.AddChild(attackSfxNode);
-        assets.AddChild(hitSfxNode);
+        assets.AddChild(sprite);
+        assets.AddChild(attackSfx);
+        assets.AddChild(hitSfx);
         assets.SetDisplayFolded(true);
 
+        sprite.UniqueNameInOwner = true;
+        attackSfx.UniqueNameInOwner = true;
+        hitSfx.UniqueNameInOwner = true;
 
-        spriteNode.SetOwner(GetTree().GetEditedSceneRoot());
-        playerNode.SetOwner(GetTree().GetEditedSceneRoot());
-        assets.SetOwner(GetTree().GetEditedSceneRoot());
-        attackSfxNode.SetOwner(GetTree().GetEditedSceneRoot());
-        hitSfxNode.SetOwner(GetTree().GetEditedSceneRoot());
+        this.EditorAddChild(assets);
+        this.EditorAddChild(animationPlayer);
     }
 }
