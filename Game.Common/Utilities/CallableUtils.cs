83829e43b65d9c0af6e4fb8767ecdbbb6f6d5356
dev-laww <lawrenceallen.tech@gmail.com>
2025-02-18 02:40:30 +0800

    feat: add CallableUtils for handling Godot delegates

---
+++ Game.Common/Utilities/CallableUtils.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace Game.Common.Utilities;
+
+public static class CallableUtils
+{
+    public static Callable FromMethod(Delegate action)
+    {
+        if (action.Target is GodotObject) return Callable.From((Action)action);
+
+        var err = new InvalidOperationException("Action target is not a Godot object.");
+        Log.Error(err);
+
+        throw err;
+    }
+}
