9653bf8feb3bb33bc480fd2212d7d8949ddaf048
dev-laww <lawrenceallen.tech@gmail.com>
2025-02-18 02:44:39 +0800

    feat: expose Instance property and add signal connection methods in AutoLoad class

--- Scripts/AutoLoad/AutoLoad.cs
+++ Scripts/AutoLoad/AutoLoad.cs
@@ -1,10 +1,11 @@
+using Game.Common;
 using Godot;
 
 namespace Game.AutoLoad;
 
 public abstract partial class AutoLoad<T> : Node where T : AutoLoad<T>
 {
-    protected static T Instance { get; private set; }
+    public static T Instance { get; private set; }
 
     public override void _EnterTree()
     {
@@ -17,5 +18,39 @@ public abstract partial class AutoLoad<T> : Node where T : AutoLoad<T>
     {
         Instance?.QueueFree();
         Instance = null;
+    }
+
+    public static void ConnectToSignal(StringName signal, Callable target, uint flags = 0)
+    {
+        if (Instance == null)
+        {
+            Log.Warn($"{typeof(T).Name} is not loaded.");
+            return;
+        }
+
+        if (Instance.IsConnected(signal, target))
+        {
+            Log.Warn($"{typeof(T).Name} is already connected to {signal}.");
+            return;
+        }
+
+        Instance.Connect(signal, target, flags);
+    }
+
+    public static void DisconnectFromSignal(StringName signal, Callable target)
+    {
+        if (Instance == null)
+        {
+            Log.Warn($"{typeof(T).Name} is not loaded.");
+            return;
+        }
+
+        if (!Instance.IsConnected(signal, target))
+        {
+            Log.Warn($"{typeof(T).Name} is not connected to {signal}.");
+            return;
+        }
+
+        Instance.Disconnect(signal, target);
     }
 }
