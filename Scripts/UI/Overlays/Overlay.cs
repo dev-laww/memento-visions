c5d4ba43bf933db366f579156919094fa13e94fc
dev-laww <lawrenceallen.tech@gmail.com>
2025-02-20 02:08:48 +0800

    refactor: updated root nodes to follow recent changes

--- Scripts/UI/Overlays/Overlay.cs
+++ Scripts/UI/Overlays/Overlay.cs
@@ -1,41 +1,12 @@
-using Game.Components;
-using Game.Utils.Extensions;
 using Godot;
 
 namespace Game.UI.Overlays;
 
 // TODO: animate open/close
-public abstract partial class Overlay : Control
+public abstract partial class Overlay : CanvasLayer
 {
-    public void Toggle()
-    {
-        if (Visible)
-            Close();
-        else
-            Open();
-    }
-
     public virtual void Close()
     {
-        this.GetPlayer()?.InputManager.RemoveLock();
-
-        SetProcessInput(false);
-        SetProcessUnhandledInput(false);
-        MouseFilter = MouseFilterEnum.Pass;
-
-        if (GameManager.CurrentOverlay == this) GameManager.CurrentOverlay = null;
-
-        Hide();
-    }
-
-    public virtual void Open()
-    {
-        this.GetPlayer()?.InputManager.AddLock();
-
-        Show();
-        SetProcessInput(true);
-        SetProcessUnhandledInput(true);
-
-        MouseFilter = MouseFilterEnum.Stop;
+        QueueFree();
     }
 }
