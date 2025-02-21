81cf490c31f372d18df2a060bc151ea13cf37b31
dev-laww <lawrenceallen.tech@gmail.com>
2025-02-22 01:24:03 +0800

    feat: initial cinematic screen

---
+++ Scripts/UI/Screens/Cinematic.cs
@@ -0,0 +1,36 @@
+using Godot;
+using GodotUtilities;
+
+namespace Game.UI.Screens;
+
+[Scene]
+public partial class Cinematic : CanvasLayer
+{
+    private const string ANIM_IN = "in";
+
+    [Node] private AnimationPlayer animationPlayer;
+    [Node] private ColorRect topBar;
+    [Node] private ColorRect bottomBar;
+
+    public override void _Notification(int what)
+    {
+        if (what != NotificationSceneInstantiated) return;
+
+        WireNodes();
+    }
+
+    public void Start()
+    {
+        animationPlayer.Play(ANIM_IN);
+    }
+
+    public async void Stop()
+    {
+        animationPlayer.PlayBackwards(ANIM_IN);
+
+        await ToSignal(animationPlayer, "animation_finished");
+
+        // QueueFree();
+    }
+}
+
