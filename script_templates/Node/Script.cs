091cd4f5826aeb0f1313579e1f0665f590f025ca
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-16 01:23:20 +0800

    feat: normal script template

---
+++ script_templates/Node/Script.cs
@@ -0,0 +1,15 @@
+// meta-name: Base Script
+// meta-description: Predefined template for a script
+// meta-default: true
+// meta-space-indent: 4
+
+
+using _BINDINGS_NAMESPACE_;
+
+
+namespace Game;
+
+public partial class _CLASS_ : _BASE_
+{
+
+}
