403810152ea56340b7436986f09b99a2134d8b96
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-17 19:31:38 +0800

    feat: implement LineDamage and DamageFactory for enhanced damage handling

--- Scripts/Components/Battle/Damage/LineDamage.cs
+++ Scripts/Components/Battle/Damage/LineDamage.cs
@@ -7,19 +7,44 @@ namespace Game.Components;
 public partial class LineDamage : Node2D
 {
     [Node] private Timer timer;
-
-    [Node] private HitBox hitBox;
+    [Node] public HitBox HitBox;
     [Node] private CollisionShape2D collisionShape2D;
-
-    [Export] private Vector2 endPosition;
-
-    private LineTelegraph telegraph;
 
     public override void _Notification(int what)
     {
         if (what != NotificationSceneInstantiated) return;
 
         WireNodes();
+    }
+
+    public override void _Ready()
+    {
+        collisionShape2D.Disabled = true;
+
+        timer.Timeout += OnTimerTimeout;
+    }
+
+    public void Start(Vector2 start, Vector2 end, float duration = -1)
+    {
+        GlobalPosition = start;
+
+        var trajectory = end - start;
+        var length = trajectory.Length();
+        HitBox.Rotation = trajectory.Angle();
+
+        var shape = (RectangleShape2D)collisionShape2D.Shape;
+
+        shape.Size = new Vector2(length, 16);
+
+        collisionShape2D.Position = new Vector2(length / 2, 0);
+        timer.Start(duration);
+    }
+
+    public void OnTimerTimeout()
+    {
+        collisionShape2D.Disabled = false;
+
+        GetTree().CreateTimer(0.1f).Timeout += QueueFree;
     }
 }
 
---
+++ Scripts/Utils/DamageFactory.cs
@@ -0,0 +1,61 @@
+using Game.Components;
+using Game.Entities;
+using Game.Utils.Battle;
+using Godot;
+
+
+namespace Game.Utils;
+
+public static class DamageFactory
+{
+    private static readonly PackedScene LineDamageScene = ResourceLoader.Load<PackedScene>("res://Scenes/Components/Battle/Damage/LineDamage.tscn");
+    private static readonly PackedScene CircleDamageScene = ResourceLoader.Load<PackedScene>("res://Scenes/Components/Battle/Damage/CircleDamage.tscn");
+
+    public class LineDamageBuilder(Vector2 start, Vector2 end)
+    {
+        private readonly Vector2 start = start;
+        private readonly Vector2 end = end;
+        private float duration = -1;
+        private Entity owner;
+        private Attack.Type type;
+        private float damage = 1;
+
+        public LineDamageBuilder SetOwner(Entity owner)
+        {
+            this.owner = owner;
+            return this;
+        }
+
+        public LineDamageBuilder SetDuration(float duration)
+        {
+            this.duration = duration;
+            return this;
+        }
+
+        public LineDamageBuilder SetDamage(float damage)
+        {
+            this.damage = damage;
+            return this;
+        }
+
+        public LineDamageBuilder SetDamageType(Attack.Type type)
+        {
+            this.type = type;
+            return this;
+        }
+
+        public LineDamage Build()
+        {
+            var instance = LineDamageScene.Instantiate<LineDamage>();
+
+            instance.HitBox.Type = type;
+            instance.HitBox.Damage = damage;
+            instance.HitBox.HitboxOwner = owner;
+
+            GameManager.CurrentScene.AddChild(instance);
+
+            instance.Start(start, end, duration);
+            return instance;
+        }
+    }
+}
