45a76fe90d109b4d05ff63a5f3d534c27dc5b75d
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-17 21:11:03 +0800

    feat: status effect registry

--- Game.Common/Constants.cs
+++ Game.Common/Constants.cs
@@ -8,4 +8,5 @@ public static class Constants
     public const string ENEMIES_PATH = "res://Scenes/Entities/Enemies";
     public const string SAVE_NAME = "save.json";
     public const string ENTITIES_PATH = "res://Scenes/Entities";
+    public const string STATUS_EFFECTS_PATH = "res://Scenes/StatusEffects";
 }
