97d7baa12badba4c862d50fafe0164ab96149f6d
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-17 11:22:14 +0800

    fix: handle potential null reference in CurrentScene property

--- Scripts/Components/Managers/GameManager.cs
+++ Scripts/Components/Managers/GameManager.cs
@@ -24,7 +24,7 @@ public partial class GameManager : Node
 
     private bool isDevConsoleOpen;
 
-    public static Node CurrentScene => instance.currentScene.GetChildren().FirstOrDefault() ?? instance.GetTree().CurrentScene;
+    public static Node CurrentScene => instance?.currentScene.GetChildren().FirstOrDefault() ?? GameCamera.Instance.GetTree().CurrentScene;
 
     public static Overlay CurrentOverlay
     {
