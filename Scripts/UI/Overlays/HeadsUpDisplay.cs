a245c416659bcd50202ffac358bb2506d625e85b
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-21 02:52:35 +0800

    feat: initial hud

--- Scripts/UI/Overlays/HeadsUpDisplay.cs
+++ Scripts/UI/Overlays/HeadsUpDisplay.cs
@@ -1,15 +1,35 @@
+using Game.Entities;
+using Game.UI.Common;
+using Game.Utils.Extensions;
 using Godot;
 using GodotUtilities;
 
 namespace Game.UI.Overlays;
 
-// [Scene]
+[Scene]
 public partial class HeadsUpDisplay : Overlay
 {
-    // public override void _Notification(int what)
-    // {
-    //     if (what != NotificationSceneInstantiated) return;
-    //
-    //     WireNodes();
-    // }
+    [Node] private HealthBar healthBar;
+
+    private Player player;
+
+    public override void _Notification(int what)
+    {
+        if (what != NotificationSceneInstantiated) return;
+
+        WireNodes();
+    }
+
+    public override void _Ready()
+    {
+        player = this.GetPlayer();
+        healthBar.Initialize(player.StatsManager);
+
+        player.StatsManager.LevelUp += OnLevelUp;
+    }
+
+    private void OnLevelUp(float _)
+    {
+        healthBar.Initialize(player.StatsManager);
+    }
 }
