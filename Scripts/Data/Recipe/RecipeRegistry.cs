9a36a2fb85b6df5b714f614030104c08fa758fad
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-25 02:05:53 +0800

    fix: recipe registry not loading recipes

--- Scripts/Data/Recipe/RecipeRegistry.cs
+++ Scripts/Data/Recipe/RecipeRegistry.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Game.Common;
 using Game.Common.Abstract;
+using Game.Common.Utilities;
 using Godot;
 
 namespace Game.Data;
@@ -14,4 +15,22 @@ public partial class RecipeRegistry : Registry<Recipe, RecipeRegistry>
     public static List<Recipe> GetRecipes(Recipe.Type type) => Resources.Values
         .Where(resource => resource.RecipeType == type)
         .ToList();
+
+    protected override void LoadResources()
+    {
+        var files = DirAccessUtils.GetFilesRecursively(Instance.Value.ResourcePath);
+
+        foreach (var file in files)
+        {
+            if (!file.EndsWith(".tres") && !file.EndsWith(".tres.remap")) continue;
+
+            var resource = ResourceLoader.Load<Recipe>(file);
+
+            if (resource == null) continue;
+
+            var id = resource.Result.Item.Id;
+
+            Resources[id] = resource;
+        }
+    }
 }
