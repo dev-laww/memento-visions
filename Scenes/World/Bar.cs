1455067088f509bdfd6066435067fe36bc2b6b82
crimson <ambilllKylee@gmail.com>
2025-03-26 16:59:34 +0800

    Move resources to the proper folder

---
+++ Scenes/World/Bar.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class Bar : Node2D
+{
+    private Quest quest = ResourceLoader.Load<Quest>("res://resources/quests/Prologue/NightofShadows.tres");
+    
+    public override void _Notification(int what)
+    {
+        if (what != NotificationSceneInstantiated) return;
+
+        WireNodes();
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+    
+    
+    public void CompleteObjectiveAtIndex(int index)
+    {
+        if (quest == null)
+        {
+            GD.PrintErr("Quest not loaded");
+            return;
+        }
+        
+        quest.CompleteObjective(1);
+    }
+
+}
