e24955c4df79aa8718b182f409835f0802cd0cc5
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-30 21:00:20 +0800

    feat: dummy update

--- Scripts/Entities/Enemies/Common/Dummy.cs
+++ Scripts/Entities/Enemies/Common/Dummy.cs
@@ -9,6 +9,7 @@ namespace Game.Entities;
 public partial class Dummy : Enemy
 {
     [Node] private AnimationPlayer animationPlayer;
+    [Node] private AnimatedSprite2D smoothAnimatedSprite2d;
     [Node] private HealthNumberManager healthNumberManager;
 
     public override void _Notification(int what)
@@ -21,12 +22,18 @@ public partial class Dummy : Enemy
     public override void OnReady()
     {
         StatsManager.AttackReceived += OnAttackReceived;
+        animationPlayer.AnimationFinished += _ => animationPlayer.Play("idle");
     }
 
     protected override void Die(DeathInfo info) { }
 
     private void OnAttackReceived(Attack attack)
     {
+        var sourcePosition = attack.Source.GlobalPosition;
+
+        var directionToSource = (sourcePosition - GlobalPosition).Normalized();
+        smoothAnimatedSprite2d.FlipH = directionToSource.X > 0;
+
         animationPlayer.Stop();
 
         animationPlayer.Play(attack.Critical ? "crit" : "hit");
