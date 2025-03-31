9bd23b88f9e77719a571172c56ab4b605c943021
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-31 16:36:29 +0800

    feat: aghon animation state machines

--- Scripts/Entities/Enemies/Boss/Aghon.cs
+++ Scripts/Entities/Enemies/Boss/Aghon.cs
@@ -6,6 +6,8 @@ namespace Game.Entities;
 [Scene]
 public partial class Aghon : Entity
 {
+    private int phase = 1;
+
     public override void _Notification(int what)
     {
         if (what != NotificationSceneInstantiated) return;
