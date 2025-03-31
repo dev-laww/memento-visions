8a3e6ce5bad82cea1f394046a057ec3f074a6439
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-31 20:49:16 +0800

    feat: add Pause and Resume methods to TimerExtensions for improved timer control

--- Game.Common/Extensions/TimerExtensions.cs
+++ Game.Common/Extensions/TimerExtensions.cs
@@ -4,9 +4,17 @@ namespace Game.Common.Extensions;
 
 public static class TimerExtensions
 {
-    public static void Reset(this Timer timer)
+    public static void Pause(this Timer timer)
     {
-        timer.Stop();
-        timer.Start();
+        if (timer.IsStopped()) return;
+
+        timer.Paused = true;
+    }
+
+    public static void Resume(this Timer timer)
+    {
+        if (!timer.Paused) return;
+
+        timer.Paused = false;
     }
 }
