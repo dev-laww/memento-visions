d5d418830c775705b90575c556c66d5c92a78b51
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-31 19:43:17 +0800

    feat: add events for enemy registration and unregistration in EnemyManager

--- Scripts/Autoload/EnemyManager.cs
+++ Scripts/Autoload/EnemyManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Game.Common;
 using Game.Common.Utilities;
 using Game.Entities;
@@ -13,6 +15,10 @@ public partial class EnemyManager : Autoload<EnemyManager>
 
     public static IReadOnlyList<Enemy> Enemies => Instance.enemies;
     public static int EnemyCount => Instance.enemies.Count;
+    public static event Action<Enemy> EnemyRegistered;
+    public static event Action<Enemy> EnemyUnregistered;
+
+    public static IReadOnlyList<Enemy> EnemiesOfType(Enemy.EnemyType type) => [.. Instance.enemies.Where(enemy => enemy.Type == type)];
 
     public static void Register(Entity.SpawnInfo info)
     {
@@ -25,6 +31,7 @@ public partial class EnemyManager : Autoload<EnemyManager>
         info.Entity.AddToGroup();
         info.Entity.AddToGroup("Enemy");
         Instance.enemies.Add(enemy);
+        EnemyRegistered?.Invoke(enemy);
 
         Log.Debug($"{enemy} added to the registry. {info}");
     }
@@ -32,13 +39,8 @@ public partial class EnemyManager : Autoload<EnemyManager>
     public static void Unregister(Entity.DeathInfo info)
     {
         Instance.enemies.Remove(info.Victim as Enemy);
+        EnemyUnregistered?.Invoke(info.Victim as Enemy);
 
         Log.Debug($"{info.Victim} removed from the registry. {info}");
-    }
-
-    [Command(Name = "test", Description = "Test command.")]
-    private void Test(string name)
-    {
-        GD.Print($"Hello, {name}!");
     }
 }
