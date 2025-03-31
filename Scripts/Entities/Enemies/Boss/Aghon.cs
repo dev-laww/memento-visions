0c43d37cbfe44edeaf067d2cc4080a9158d1aa72
dev-laww <lawrenceallen.tech@gmail.com>
2025-03-31 22:09:44 +0800

    aghon common attack for first and second phase

--- Scripts/Entities/Enemies/Boss/Aghon.cs
+++ Scripts/Entities/Enemies/Boss/Aghon.cs
@@ -1,4 +1,6 @@
+using Game.Common.Extensions;
 using Game.Components;
+using Game.Utils;
 using Game.Utils.Extensions;
 using Godot;
 using GodotUtilities;
@@ -8,8 +10,8 @@ namespace Game.Entities;
 [Scene]
 public partial class Aghon : Enemy
 {
+    private const string START_RANDOM = "start_random";
     private const string TRANSFORM = "transform";
-    private const string MOVE = "move";
     private const string COMMON_ATTACK = "common_attack";
     private const string SPECIAL_ATTACK_1 = "special_attack_1";
     private const string SPECIAL_ATTACK_2 = "special_attack_2";
@@ -17,6 +19,7 @@ public partial class Aghon : Enemy
     [Node] private AnimationTree animationTree;
     [Node] private VelocityManager velocityManager;
     [Node] private PathFindManager pathFindManager;
+    [Node] private Timer commonAttackTimer;
 
 
     private AnimationNodeStateMachinePlayback playback;
@@ -34,10 +37,9 @@ public partial class Aghon : Enemy
         playback = (AnimationNodeStateMachinePlayback)animationTree.Get("parameters/playback");
 
         StateMachine.AddStates(Normal, EnterNormal, ExitNormal);
-        StateMachine.AddStates(CommonAttack);
-        StateMachine.AddStates(SpecialAttack1);
-        StateMachine.AddStates(SpecialAttack2);
-        StateMachine.AddStates(TransformToSecondPhase);
+        StateMachine.AddStates(TravelToPlayer, EnterTravelToPlayer);
+        StateMachine.AddStates(CommonAttack, EnterCommonAttack, ExitCommonAttack);
+        StateMachine.AddStates(TransformToSecondPhase, EnterTransformToSecondPhase);
 
         StateMachine.SetInitialState(Normal);
     }
@@ -58,7 +60,7 @@ public partial class Aghon : Enemy
             return;
         }
 
-        StatsManager.Heal(100, StatsManager.ModifyMode.Percentage); // TODO: change to transform state instead of heal
+        StateMachine.ChangeState(TransformToSecondPhase);
     }
 
     #region States
@@ -66,36 +68,87 @@ public partial class Aghon : Enemy
     {
         pathFindManager.SetTargetPosition(this.GetPlayer()?.GlobalPosition ?? GlobalPosition);
         pathFindManager.Follow();
+
+        if (commonAttackTimer.IsStopped())
+        {
+            StateMachine.ChangeState(TravelToPlayer);
+        }
     }
 
     private void EnterNormal()
     {
-
+        commonAttackTimer.Resume();
     }
 
     private void ExitNormal()
     {
-
+        commonAttackTimer.Pause();
     }
 
-    private void CommonAttack()
+    private void TravelToPlayer()
     {
+        var distanceToTarget = GlobalPosition.DistanceSquaredTo(pathFindManager.GetTargetPosition());
+        pathFindManager.Follow();
 
+        if (!pathFindManager.NavigationAgent2D.IsNavigationFinished() && distanceToTarget > 32 * 32) return;
+
+        StateMachine.ChangeState(CommonAttack);
     }
 
-    private void SpecialAttack1()
+    private void EnterTravelToPlayer()
     {
-
+        pathFindManager.SetTargetPosition(this.GetPlayer()?.GlobalPosition ?? GlobalPosition);
     }
 
-    private void SpecialAttack2()
+    private async void CommonAttack()
     {
+        await ToSignal(animationTree, "animation_finished");
 
+        StateMachine.ChangeState(Normal);
     }
 
-    private void TransformToSecondPhase()
+    private void EnterCommonAttack()
     {
+        playback.Travel(COMMON_ATTACK);
 
+        new DamageFactory.HitBoxBuilder(GlobalPosition)
+            .SetDamage(StatsManager.Damage)
+            .SetDelay(.3f)
+            .SetShape(new CircleShape2D { Radius = 40 })
+            .SetOwner(this)
+            .Build();
+
+        new DamageFactory.HitBoxBuilder(GlobalPosition)
+            .SetDamage(StatsManager.Damage)
+            .SetDelay(.7f)
+            .SetShape(new CircleShape2D { Radius = 40 })
+            .SetOwner(this)
+            .Build();
+    }
+
+    private void ExitCommonAttack()
+    {
+        commonAttackTimer.Call(START_RANDOM);
+    }
+
+    private async void TransformToSecondPhase()
+    {
+        await ToSignal(animationTree, "animation_finished");
+        await ToSignal(GetTree().CreateTimer(1f), "timeout");
+
+        StateMachine.ChangeState(Normal);
+        StatsManager.SetInvulnerable(false);
+    }
+
+    private void EnterTransformToSecondPhase()
+    {
+        playback.Travel(TRANSFORM);
+        StatsManager.SetInvulnerable(true);
+
+        StatsManager.Heal(100, StatsManager.ModifyMode.Percentage);
+        StatsManager.IncreaseDamage(40, StatsManager.ModifyMode.Percentage);
+        StatsManager.DecreaseDefense(100, StatsManager.ModifyMode.Percentage);
+        StatsManager.ApplySpeedModifier("second_phase", .15f);
     }
     #endregion
 
