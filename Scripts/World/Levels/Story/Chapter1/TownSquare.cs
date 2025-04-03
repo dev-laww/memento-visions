4442def99fa779d6def9affeb185efea5746779b
dev-laww <lawrenceallen.tech@gmail.com>
2025-04-03 20:40:11 +0800

    fix: update enemy scene references and clean up InteractionUI node structure

--- Scripts/World/Levels/Story/Chapter1/TownSquare.cs
+++ Scripts/World/Levels/Story/Chapter1/TownSquare.cs
@@ -1,9 +1,5 @@
 using Godot;
-using System;
-using Game.Autoload;
-using Game.Components;
 using Game.Entities;
-using Game.UI.Screens;
 using GodotUtilities;
 
 namespace Game.Levels.Story;
@@ -11,12 +7,11 @@ namespace Game.Levels.Story;
 [Scene]
 public partial class TownSquare : Node2D
 {
-    [Node] private Spawner Spawner;
     [Node] private Entity Chief;
     [Node] private AnimationPlayer AnimationPlayer;
-    
-    
-    
+
+
+
     public override void _Notification(int what)
     {
         if (what == NotificationSceneInstantiated)
@@ -25,36 +20,15 @@ public partial class TownSquare : Node2D
         }
     }
 
-    public override void _Ready()
-    {
-        base._Ready();
-        InitializeSpawnPoints(); 
-       GD.Print("Chief position: ", Chief.Position);
-    }
-    
-    private void InitializeSpawnPoints()
-    { 
-        Spawner.SpawnPoints.Clear();
-        
-        Spawner.SpawnPoints.Add(new Vector2(450, 257));
-    }
-    public async void PlayFadeAnimation()
+    public void PlayFadeAnimation()
     {
         AnimationPlayer.Play("Fade");
     }
+
     public void Spawn()
     {
-        Random random = new Random();
-        for (int i = 0; i < 8; i++)
-        {
-            foreach (var enemy in Spawner.Spawn())
-            {
-                Vector2 offset = new Vector2(random.Next(-3, 4), random.Next(-3, 4));
-                enemy.Position += offset;
-                AddChild(enemy);
-            }
-        }
+        // TODO: spawn enemies
     }
-    
-    public void RuntoMarker(){}
+
+    public void RuntoMarker() { }
 }
