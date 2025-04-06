954d98438b31450dbab35407f22437d241e7b89c
dev-laww <lawrenceallen.tech@gmail.com>
2025-04-06 17:37:42 +0800

    feat: witch npc

---
+++ Scripts/Entities/Characters/Witch.cs
@@ -0,0 +1,43 @@
+using Godot;
+using GodotUtilities;
+
+namespace Game.Entities;
+
+[Scene]
+public partial class Witch : Entity
+{
+    [Node] private Timer tapTimer;
+    [Node] private AnimationTree animationTree;
+
+    private AnimationNodeStateMachinePlayback playback;
+
+    public override void _Notification(int what)
+    {
+        if (what != NotificationSceneInstantiated) return;
+
+        WireNodes();
+    }
+
+    public override void OnReady()
+    {
+        playback = (AnimationNodeStateMachinePlayback)animationTree.Get("parameters/playback");
+
+        tapTimer.Timeout += OnTapTimeout;
+    }
+
+    private void OnTapTimeout()
+    {
+        playback.Travel("tap");
+        tapTimer.Call("start_random");
+    }
+
+    public void Work()
+    {
+        playback.Travel("work");
+    }
+
+    public void EndWork()
+    {
+        playback.Travel("idle");
+    }
+}
