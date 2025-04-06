0c36b9e626957f9f775cefb98e5cd06c889049eb
crimson <ambilllKylee@gmail.com>
2025-04-06 11:44:52 +0800

    feat: rename Chief to Mayor and update quest trigger interactions add spawn for boss Aghon

--- Scripts/World/Levels/Story/Chapter1/TownSquare.cs
+++ Scripts/World/Levels/Story/Chapter1/TownSquare.cs
@@ -1,3 +1,4 @@
+using Game.Components;
 using Godot;
 using Game.Entities;
 using GodotUtilities;
@@ -7,9 +8,9 @@ namespace Game.Levels.Story;
 [Scene]
 public partial class TownSquare : Node2D
 {
-    [Node] private Entity Chief;
+    [Node] private Entity Mayor;
     [Node] private AnimationPlayer AnimationPlayer;
-
+    [Node] private Spawner Spawner;
 
 
     public override void _Notification(int what)
@@ -27,7 +28,9 @@ public partial class TownSquare : Node2D
 
     public void Spawn()
     {
-        // TODO: spawn enemies
+        Mayor.Visible = false;
+        Vector2 specificPosition = new Vector2(437, 259);
+        Spawner.SpawnBoss(specificPosition);
     }
 
     public void RuntoMarker() { }
