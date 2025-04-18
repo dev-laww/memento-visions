33d558f894111d757030deb3b18cb596626441e9
crimson <ambilllKylee@gmail.com>
2025-04-18 21:09:15 +0800

    fix: streamline cutscene management and quest handling in BossLevel

--- Scripts/World/Levels/Story/Chapter2/BossLevel.cs
+++ Scripts/World/Levels/Story/Chapter2/BossLevel.cs
@@ -8,14 +8,13 @@ using GodotUtilities;
 namespace Game.World;
 
 [Scene]
-
 public partial class BossLevel : BaseLevel
 {
     [Node] private Node2D enemy;
     [Node] private ResourcePreloader resourcePreloader;
-    [Node] private Marker2D cinematicPosition1, cinematicPosition2,lunariaSpawnPoint;
-    private Quest quest = QuestRegistry.Get("res://resources/quests/chapter_2/engkanto_wrath.tres");
-    
+    [Node] private Marker2D cinematicPosition1, lunariaSpawnPoint;
+    [Node] private ScreenMarker screenMarker;
+
     public override void _Notification(int what)
     {
         if (what != NotificationSceneInstantiated) return;
@@ -26,25 +25,30 @@ public partial class BossLevel : BaseLevel
     public override void _Ready()
     {
         base._Ready();
+        screenMarker.Toggle(false);
         QuestManager.QuestUpdated += OnQuestUpdated;
-        StartCutscene(cinematicPosition1.GlobalPosition);
-        StartCutscene(cinematicPosition2.GlobalPosition);
+        StartCutscene();
     }
-    
+
     private void OnQuestUpdated(Quest quest)
     {
-        if (this.quest.Objectives[0].Completed)
-        {
-            SpawnLunaria();
-            QuestManager.QuestUpdated -= OnQuestUpdated;
-        }
+        if (quest.Id != "quest:engkanto_wrath" || !quest.Objectives[0].Completed) return;
+        CinematicManager.StartCinematic();
+        MoveCameraTo(lunariaSpawnPoint.GlobalPosition, 1f,
+            () => { CinematicManager.EndCinematic(); });
+        SpawnLunaria();
+        QuestManager.QuestUpdated -= OnQuestUpdated;
     }
-    
-    public void StartCutscene(Vector2 targetPosition)
+
+    public void StartCutscene()
     {
         CinematicManager.StartCinematic();
-        MoveCameraTo(targetPosition, 2.5f,
-            () => {  CinematicManager.EndCinematic(); });
+        MoveCameraTo(cinematicPosition1.GlobalPosition, 2.5f,
+            () =>
+            {
+                MoveCameraTo(lunariaSpawnPoint.GlobalPosition, 2.5f,
+                    () => { CinematicManager.EndCinematic(); });
+            });
     }
 
     public static void MoveCameraTo(Vector2 position, float duration, Action onComplete = null)
@@ -53,19 +57,18 @@ public partial class BossLevel : BaseLevel
         var timer = GameCamera.Instance.GetTree().CreateTimer(duration);
         timer.Timeout += () => { onComplete?.Invoke(); };
     }
-    
+
     public void SpawnLunaria()
     {
         var lunaria = resourcePreloader.InstanceSceneOrNull<Lunaria>();
         lunaria.GlobalPosition = lunariaSpawnPoint.GlobalPosition;
         enemy.AddChild(lunaria);
     }
-    
+
     public override void _ExitTree()
     {
         base._ExitTree();
         QuestManager.QuestUpdated -= OnQuestUpdated;
         enemy.QueueFree();
     }
-    
 }
